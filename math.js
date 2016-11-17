8667dd23745a0946744a97e7df623ac8383528b5
verefor <verefor@googlemail.com>
2016-11-17 11:00:25 +0000

    created matrix add function

--- math.js
+++ math.js
@@ -1,2 +1,28 @@
-var x=5
-console.log(x);
+_math={};
+
+_math.matrix={};
+_math.matrix.add=function(a,b){
+	if(a.length==b.length){
+		var equalcolumns=0;
+		for(var x=0;x<a.length;x++){
+			if(a[x].length==b[x].length){equalcolumns++}
+		}
+		if(equalcolumns==a.length){
+			var c=[];
+			for(var x=0;x<a.length;x++){
+				c[x]=[];
+				for(var y=0;y<a[x].length;y++){
+					c[x][y]=a[x][y]+b[x][y];
+				}
+			}
+			return c;
+		}
+		else{
+			console.log("Matrices do no have an equal number of columns")
+		}
+	}
+	else{
+		console.log("Matrices do not have an equal number of rows")
+	}
+	
+}

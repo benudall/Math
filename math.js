f4b889feecfa21e0d4991557ec93e554f38a1f04
verefor <verefor@googlemail.com>
2016-11-21 15:36:32 +0000

    added in dotproduct, matrix analyse, matrix transpose and matrix multiply

--- math.js
+++ math.js
@@ -1,27 +1,84 @@
-Math.matrix={};
-Math.matrix.add=function(a,b){
+Math.dotproduct=function(a,b){
 	if(a.length==b.length){
-		var equalcolumns=0;
+		dotproduct=0;
 		for(var x=0;x<a.length;x++){
-			if(a[x].length==b[x].length){equalcolumns++}
+			dotproduct+=a[x]*b[x];
 		}
-		if(equalcolumns==a.length){
-			var c=[];
-			for(var x=0;x<a.length;x++){
-				c[x]=[];
-				for(var y=0;y<a[x].length;y++){
-					c[x][y]=a[x][y]+b[x][y];
+		return dotproduct;
+	}
+	else{
+		console.log("Arrays are not equal length");
+		return;
+	}
+}
+Math.matrix={};
+Math.matrix.analyse=function(a){
+	if(Array.isArray(a)){
+		var a={matrix:a};
+		a.rows=a.matrix.length;
+		a.cols=a.matrix[0].length;
+		for(var x=1;x<a.rows;x++){
+			if(a.cols!=a.matrix[x].length){
+				console.log("Inconsistent number of columns in matrix");
+				return;
+			}
+		}
+		return a;
+	}
+	else if(!Array.isArray(a) && typeof(a)=="object"){
+		return a;
+	}
+}
+Math.matrix.transpose=function(a){
+	var a=Math.matrix.analyse(a);
+	transpose=[];
+	for(var x=0;x<a.cols;x++){
+		transpose[x]=[];
+		for(var y=0;y<a.rows;y++){
+			transpose[x][y]=a.matrix[y][x];
+		}
+	}
+	return transpose;
+}
+Math.matrix.add=function(a,b){
+	var a=Math.matrix.analyse(a);
+	var b=Math.matrix.analyse(b);
+	if(a.rows==b.rows){
+		if(a.cols==b.cols){
+			var add=[];
+			for(var x=0;x<a.rows;x++){
+				add[x]=[];
+				for(var y=0;y<b.cols;y++){
+					add[x][y]=a.matrix[x][y]+b.matrix[x][y];
 				}
 			}
-			return c;
+			return add;
 		}
 		else{
-			console.log("Matrices do no have an equal number of columns")
+			console.log("Matrices do not have an equal number of columns");
+			return;
 		}
 	}
 	else{
-		console.log("Matrices do not have an equal number of rows")
+		console.log("Matrices do not have an equal number of rows");
+		return;
 	}
 	
 }
-//multiply
+Math.matrix.multiply=function(a,b){
+	var a=Math.matrix.analyse(a);
+	var b=Math.matrix.analyse(b);
+	if(a.cols==b.rows){
+		multiply=[];
+		for(var x=0;x<a.rows;x++){
+			multiply[x]=[];
+			for(var y=0;y<b.cols;y++){
+				multiply[x][y]=Math.dotproduct(a.matrix[x],Math.matrix.transpose(b)[y]);
+			}
+		}
+		return multiply;
+	}
+	else{
+		console.log("Matrices are not mulitplicable")
+	}
+}
